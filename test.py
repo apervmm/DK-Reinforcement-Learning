52e71334dd4ade6318f5b614e8a26909e4b3cbb0
apervmm <apervmm@gmail.com>
2025-05-06 14:59:34 -0700

    upd

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ wrapped_env.observation_space.shape
 
 observation, info = env.reset(seed=42)
 for _ in range(1000):
-   action = env.action_space.sample()  # this is where you would insert your policy
+   action = env.action_space.sample()
    observation, reward, terminated, truncated, info = env.step(action)
 
    if terminated or truncated:
