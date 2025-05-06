7d388db8dd11efbf3af17314905f918d345ea2cc
SigourneyScott <sscott28@calpoly.edu>
2025-05-05 20:01:22 -0700

    Add test.py .gitignore and README

---
+++ README.txt
@@ -0,0 +1,4 @@
+Installation
+1. Install Python https://www.python.org/
+2. In the command line, run pip install gymnasium
+3. In the command line, run pip install pip install "gymnasium[atari]"
---
+++ test.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+import ale_py
+from gymnasium.wrappers import FlattenObservation
+
+env = gym.make("ALE/DonkeyKong-v5", render_mode="human")
+
+env.observation_space.shape
+(96, 96, 3)
+wrapped_env = FlattenObservation(env)
+wrapped_env.observation_space.shape
+
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+   action = env.action_space.sample()  # this is where you would insert your policy
+   observation, reward, terminated, truncated, info = env.step(action)
+
+   if terminated or truncated:
+      observation, info = env.reset()
+env.close()
