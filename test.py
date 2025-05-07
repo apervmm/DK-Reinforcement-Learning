5da415c1c37405acbe8649da5314a40de0320120
ryanhu021 <ryanhu021@gmail.com>
2025-05-06 17:16:22 -0700

    format and clean up

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 import gymnasium as gym
 import ale_py
-from gymnasium.wrappers import FlattenObservation
 
 env = gym.make("ALE/DonkeyKong-v5", render_mode="human")
 
-env.observation_space.shape(96, 96, 3)
-wrapped_env = FlattenObservation(env)
-wrapped_env.observation_space.shape
+# Uncomment the following lines to see the shape of the observation space before and after flattening
+# print(env.observation_space.shape)
+# wrapped_env = FlattenObservation(env)
+# print(wrapped_env.observation_space.shape)
 
 observation, info = env.reset(seed=42)
 for _ in range(1000):
-   action = env.action_space.sample()  # this is where you would insert your policy
-   observation, reward, terminated, truncated, info = env.step(action)
+    action = env.action_space.sample()  # this is where you would insert your policy
+    observation, reward, terminated, truncated, info = env.step(action)
 
-   if terminated or truncated:
-      observation, info = env.reset()
+    if terminated or truncated:
+        observation, info = env.reset()
 env.close()
